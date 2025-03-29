c20f383813d9f10878e96dd5ca59cf318edbcc82
macioszczyk.mikolaj@gmail.com <macioszczyk.mikolaj@gmail.com>
2025-03-29 21:01:52 +0100

    added persitence

--- skierowanie/FHIR/upload-skierowanie-fhir.py
+++ skierowanie/FHIR/upload-skierowanie-fhir.py
@@ -73,7 +73,7 @@ service_request.supportingInfo[0].reference.reference = f"MedicationStatement/{m
 service_request_id = post_resource(service_request)
 print("Service Request ID:", service_request_id)
 
-# TODO: docker volume
+# TODO: caching
 # TODO: ujednolicony coding
 # TODO: posprawdzaj kody
 # TODO: przemyśl: concept a reference
