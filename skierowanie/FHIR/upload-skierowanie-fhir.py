fd41e10e8cdd609eea9471412d56ff1f6ce59dea
macioszczyk.mikolaj@gmail.com <macioszczyk.mikolaj@gmail.com>
2025-03-31 16:29:43 +0200

    added activity_definition_electromyography

--- skierowanie/FHIR/upload-skierowanie-fhir.py
+++ skierowanie/FHIR/upload-skierowanie-fhir.py
@@ -2,6 +2,7 @@ from fhir.resources.patient import Patient
 from fhir.resources.practitioner import Practitioner
 from fhir.resources.condition import Condition
 from fhir.resources.medicationstatement import MedicationStatement
+from fhir.resources.activitydefinition import ActivityDefinition
 from fhir.resources.servicerequest import ServiceRequest
 
 from fhir_utils import post_resource, create_or_get_by_identifier, load_fhir_resource
@@ -10,17 +11,21 @@ default_patient_file = "patient.json"
 default_practitioner_file = "practitioner.json"
 default_condition_parasthesia_file = "condition-parasthesia.json"
 default_medication_statement_magnesium_file = "medication-statement-magnesium.json"
+default_activity_definition_electromyography_file = "activity-definition.json"
 default_service_request_file = "service-request.json"
 
 def upload_skierowanie_full(patient_file = default_patient_file, 
                             practitioner_file = default_practitioner_file,
                             condition_parasthesia_file = default_condition_parasthesia_file, 
                             medication_statement_magnesium_file = default_medication_statement_magnesium_file, 
+                            activity_definition_electromyography_file = default_activity_definition_electromyography_file,
                             service_request_file = default_service_request_file):
+    
     patient = load_fhir_resource(patient_file, Patient)
     practitioner = load_fhir_resource(practitioner_file, Practitioner)
     condition_parasthesia = load_fhir_resource(condition_parasthesia_file, Condition)
     medication_statement_magnesium = load_fhir_resource(medication_statement_magnesium_file, MedicationStatement)
+    activity_definition_electromyography = load_fhir_resource(activity_definition_electromyography_file, ActivityDefinition)
     service_request = load_fhir_resource(service_request_file, ServiceRequest)
 
     patient_id = create_or_get_by_identifier(patient, patient.identifier[0].system)
@@ -37,10 +42,14 @@ def upload_skierowanie_full(patient_file = default_patient_file,
     medication_statement_magnesium_id = post_resource(medication_statement_magnesium)
     print("Medication Statement Magnesium ID:", medication_statement_magnesium_id)
 
+    activity_definition_electromyography_id = post_resource(activity_definition_electromyography)
+    print("Activity Definition Electromyography ID:", medication_statement_magnesium_id)
+
     service_request.subject.reference = f"Patient/{patient_id}"
     service_request.requester.reference = f"Practitioner/{practitioner_id}"
     service_request.reason[1].reference.reference = f"Condition/{condition_parasthesia_id}"
     service_request.supportingInfo[0].reference.reference = f"MedicationStatement/{medication_statement_magnesium_id}"
+    service_request.code.reference.reference = f"ActivityDefinition/{activity_definition_electromyography_id}"
 
     service_request_id = post_resource(service_request)
     print("Service Request ID:", service_request_id)
@@ -49,3 +58,4 @@ if __name__ == "__main__":
     upload_skierowanie_full()
 
 # TODO: przemyśl: concept a reference
+# Tetany - reference
