31f39fba403948ba83a3f89d99f89cee20dab670
macioszczyk.mikolaj@gmail.com <macioszczyk.mikolaj@gmail.com>
2025-04-11 14:39:07 +0200

    upload eRefferal

--- scripts/OpenEHR/upload_skierowanie_openehr.py
+++ scripts/OpenEHR/upload_skierowanie_openehr.py
@@ -1,11 +1,12 @@
+
 from openehr_conf import DATA_DIRECTORY_PATH
 from upload_composition import upload_composition
 
-MEDICAL_DOCUMENT_TYPE = "recepta"
-default_pesel = "80010112345"
-default_composition_file_name = 'ePrescription-flat.json'
+MEDICAL_DOCUMENT_TYPE = "skierowanie"
+default_pesel = "80010112346"
+default_composition_file_name = 'eRefferal-flat.json'
 default_composition_file_path = f"{DATA_DIRECTORY_PATH}{MEDICAL_DOCUMENT_TYPE}/OpenEHR/{default_composition_file_name}"
 
 if __name__ == "__main__":
-    template_id = "ePrescription"
+    template_id = "eReferral"
     upload_composition(default_pesel, default_composition_file_path, template_id)
