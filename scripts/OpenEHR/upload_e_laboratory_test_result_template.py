e48217c692f6b4fc9fa2ee72b80f810c3aaade00
macioszczyk.mikolaj@gmail.com <macioszczyk.mikolaj@gmail.com>
2025-04-12 09:39:55 +0200

    added eLaboratoryTestResult template

---
+++ scripts/OpenEHR/upload_e_laboratory_test_result_template.py
@@ -0,0 +1,8 @@
+from openehr_conf import DATA_DIRECTORY_PATH
+from upload_template import upload_template
+
+MEDICAL_DOCUMENT_TYPE = "wyniki_badan"
+default_template_file_name = 'eLaboratoryTestResult.opt'
+
+if __name__ == "__main__":
+    upload_template(template_file_name=default_template_file_name, medical_document_type=MEDICAL_DOCUMENT_TYPE)
