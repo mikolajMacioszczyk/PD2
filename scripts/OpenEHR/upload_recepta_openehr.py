8f266ed4771d921d0485fc95672fac7297f3c8d5
macioszczyk.mikolaj@gmail.com <macioszczyk.mikolaj@gmail.com>
2025-04-12 10:04:59 +0200

    upload eLaboratory test result v1

--- scripts/OpenEHR/upload_recepta_openehr.py
+++ scripts/OpenEHR/upload_recepta_openehr.py
@@ -2,10 +2,10 @@ from openehr_conf import DATA_DIRECTORY_PATH
 from composition import upload_and_save
 
 MEDICAL_DOCUMENT_TYPE = "recepta"
+TEMPLATE_ID = "ePrescription"
 default_pesel = "80010112345"
 default_composition_file_name = 'ePrescription-flat.json'
 default_composition_file_path = f"{DATA_DIRECTORY_PATH}{MEDICAL_DOCUMENT_TYPE}/OpenEHR/input/{default_composition_file_name}"
 
 if __name__ == "__main__":
-    template_id = "ePrescription"
-    upload_and_save(default_pesel, default_composition_file_path, template_id, MEDICAL_DOCUMENT_TYPE)
+    upload_and_save(default_pesel, default_composition_file_path, TEMPLATE_ID, MEDICAL_DOCUMENT_TYPE)
--- scripts/OpenEHR/upload_wyniki_badan_openehr.py
+++ scripts/OpenEHR/upload_wyniki_badan_openehr.py
@@ -2,10 +2,10 @@
 from openehr_conf import DATA_DIRECTORY_PATH
 from composition import upload_and_save
 
-MEDICAL_DOCUMENT_TYPE = "skierowanie"
-TEMPLATE_ID = "eReferral"
-default_pesel = "80010112346"
-default_composition_file_name = 'eRefferal-flat.json'
+MEDICAL_DOCUMENT_TYPE = "wyniki_badan"
+TEMPLATE_ID = "eLaboratoryTestResult"
+default_pesel = "80010112347"
+default_composition_file_name = 'eLaboratoryTestResult-flat.json'
 default_composition_file_path = f"{DATA_DIRECTORY_PATH}{MEDICAL_DOCUMENT_TYPE}/OpenEHR/input/{default_composition_file_name}"
 
 if __name__ == "__main__":
