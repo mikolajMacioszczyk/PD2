9da0633d963223a2fc88c8d6877b27ada0e29d82
macioszczyk.mikolaj@gmail.com <macioszczyk.mikolaj@gmail.com>
2025-04-17 22:09:24 +0200

    added encounter

--- scripts/FHIR/upload_pomiar.py
+++ scripts/FHIR/upload_pomiar.py
@@ -3,6 +3,7 @@ from fhir.resources.practitioner import Practitioner
 from fhir.resources.organization import Organization
 from fhir.resources.device import Device
 from fhir.resources.observation import Observation
+from fhir.resources.encounter import Encounter
 
 from fhir_utils import get_resource, post_resource, create_or_get_by_identifier, load_fhir_resource
 from file_output_fhir import save_to_output_file
@@ -13,18 +14,21 @@ default_practitioner_file = "practitioner.json"
 default_organization_file = "organization.json"
 default_device_file = "device.json"
 default_observation_file = "observation.json"
+default_encounter_file = "encounter.json"
 
 def upload_wyniki_badan_full(patient_file = default_patient_file, 
                             practitioner_file = default_practitioner_file,
                             organization_file = default_organization_file,
                             device_file = default_device_file,
-                            observation_file = default_observation_file):
+                            observation_file = default_observation_file,
+                            encounter_file = default_encounter_file):
     # Load resources
     patient = load_fhir_resource(MEDICAL_DOCUMENT_TYPE, patient_file, Patient)
     practitioner = load_fhir_resource(MEDICAL_DOCUMENT_TYPE, practitioner_file, Practitioner)
     organization = load_fhir_resource(MEDICAL_DOCUMENT_TYPE, organization_file, Organization)
     device = load_fhir_resource(MEDICAL_DOCUMENT_TYPE, device_file, Device)
     observation = load_fhir_resource(MEDICAL_DOCUMENT_TYPE, observation_file, Observation)
+    encounter = load_fhir_resource(MEDICAL_DOCUMENT_TYPE, encounter_file, Encounter)
 
     # Add resources to the server
     patient_id = create_or_get_by_identifier(patient, patient.identifier[0].system)
@@ -35,17 +39,22 @@ def upload_wyniki_badan_full(patient_file = default_patient_file,
 
     organization_id = post_resource(organization)
     print("Organization ID:", organization_id)
-    # TODO: Connect organization
 
     # TODO: Device properties
     device.owner.reference = f"Organization/{organization_id}"
     device_id = post_resource(device)
     print("Device ID:", device_id)
 
+    encounter.subject.reference = f"Patient/{patient_id}"
+    encounter.serviceProvider.reference = f"Organization/{organization_id}"
+    encounter_id = post_resource(encounter)
+    print("Encounter ID:", encounter_id)
+
     # TODO: Observation properties
     observation.subject.reference = f"Patient/{patient_id}"
     observation.device.reference = f"Device/{device_id}"
     observation.performer[0].reference = f"Practitioner/{practitioner_id}"
+    observation.encounter.reference = f"Encounter/{encounter_id}"
     observation_id = post_resource(observation)
     print("Observation ID:", observation_id)
 
