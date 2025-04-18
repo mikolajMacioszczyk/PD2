7edf879dcb4242291180d6f0a527b7a18c5408a4
macioszczyk.mikolaj@gmail.com <macioszczyk.mikolaj@gmail.com>
2025-04-18 07:58:34 +0200

    get and save pomiar bundle

--- scripts/FHIR/upload_pomiar.py
+++ scripts/FHIR/upload_pomiar.py
@@ -5,7 +5,7 @@ from fhir.resources.device import Device
 from fhir.resources.observation import Observation
 from fhir.resources.encounter import Encounter
 
-from fhir_utils import get_resource, post_resource, create_or_get_by_identifier, load_fhir_resource
+from fhir_utils import get_bundle, post_resource, create_or_get_by_identifier, load_fhir_resource
 from file_output_fhir import save_to_output_file
 
 MEDICAL_DOCUMENT_TYPE = "pomiar"
@@ -57,7 +57,13 @@ def upload_wyniki_badan_full(patient_file = default_patient_file,
     observation_id = post_resource(observation)
     print("Observation ID:", observation_id)
 
-    # TODO: Get and save
+    resource_bundle = get_bundle(Observation.__name__, observation_id, [
+        { 'name': Organization.__name__, 'id': organization_id }
+    ])
+    if resource_bundle:
+        file_name = f"bundle-{MEDICAL_DOCUMENT_TYPE}-JSON-{observation_id}.json"
+        save_to_output_file(resource_bundle, MEDICAL_DOCUMENT_TYPE, file_name)
+        print(f"Saved bundle to {file_name}")
 
 if __name__ == "__main__":
     upload_wyniki_badan_full()
