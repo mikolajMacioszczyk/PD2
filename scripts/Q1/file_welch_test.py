35fcc673511bf1230170c11467b986506254270d
macioszczyk.mikolaj@gmail.com <macioszczyk.mikolaj@gmail.com>
2025-04-22 11:10:00 +0200

    calculated welch test

--- scripts/Q1/file_welch_test.py
+++ scripts/Q1/file_welch_test.py
@@ -8,18 +8,23 @@ OUTPUT_FILE_PREFIX = "results/file_welch_test"
 if __name__ == "__main__":
     file_normal_distribution_metrics = [
         ["size_bytes", "FHIR_JSON", "less", "OpenEHR_FLAT"],
+        ["size_bytes", "FHIR_JSON", "two-sided", "OpenEHR_FLAT"],
 
         ["size_bytes", "FHIR_JSON", "less", "OpenEHR_JSON"], 
+        ["size_bytes", "FHIR_JSON", "two-sided", "OpenEHR_JSON"], 
 
         ["unique_keys", "FHIR_JSON", "less", "OpenEHR_FLAT"], 
         ["unique_keys", "FHIR_JSON", "greater", "OpenEHR_FLAT"], 
         ["unique_keys", "FHIR_JSON", "two-sided", "OpenEHR_FLAT"], 
 
         ["unique_keys", "FHIR_JSON", "greater", "OpenEHR_JSON"], 
+        ["unique_keys", "FHIR_JSON", "two-sided", "OpenEHR_JSON"], 
 
         ["avg_path_len", "FHIR_JSON", "greater", "OpenEHR_FLAT"], 
+        ["avg_path_len", "FHIR_JSON", "two-sided", "OpenEHR_FLAT"], 
 
         ["avg_path_len", "FHIR_JSON", "less", "OpenEHR_JSON"], 
+        ["avg_path_len", "FHIR_JSON", "two-sided", "OpenEHR_JSON"], 
     ]
 
     
--- scripts/Q1/graphs_welch_test.py
+++ scripts/Q1/graphs_welch_test.py
@@ -6,20 +6,49 @@ DEFAULT_FILE_NAME = "results/graphs_statistics.csv"
 OUTPUT_FILE_PREFIX = "results/graphs_welch_test"
 
 if __name__ == "__main__":
-    standards = ["FHIR", "OpenEHR"]
-    # TODO: Select metrics
+    # TODO: Max depth
     graph_normal_distribution_metrics = [
-            ["diameter", True], 
-            ["diameter", False], 
-            ["avg_path_length", True], 
-            ["avg_path_length", False], 
-            ["avg_path_from_root", True],
-            ["avg_path_from_root", False]
+            # No difference
+            ["num_nodes", "FHIR", "less", "OpenEHR"],
+            ["num_nodes", "FHIR", "greater", "OpenEHR"],
+            ["num_nodes", "FHIR", "two-sided", "OpenEHR"],
+
+            # No difference
+            ["num_edges", "FHIR", "less", "OpenEHR"],
+            ["num_edges", "FHIR", "greater", "OpenEHR"],
+            ["num_edges", "FHIR", "two-sided", "OpenEHR"],
+
+            # No difference
+            ["density", "FHIR", "less", "OpenEHR"],
+            ["density", "FHIR", "greater", "OpenEHR"],
+            ["density", "FHIR", "two-sided", "OpenEHR"],
+
+            # No difference
+            ["max_degree", "FHIR", "less", "OpenEHR"],
+            ["max_degree", "FHIR", "greater", "OpenEHR"],
+            ["max_degree", "FHIR", "two-sided", "OpenEHR"],
+
+            ["average_degree", "FHIR", "greater", "OpenEHR"],
+            ["average_degree", "FHIR", "two-sided", "OpenEHR"],
+
+            ["diameter", "FHIR", "less", "OpenEHR"],
+
+            ["avg_path_length", "FHIR", "less", "OpenEHR"],
+            ["avg_path_length", "FHIR", "two-sided", "OpenEHR"],
+
+            ["assortativity", "FHIR", "greater", "OpenEHR"],
+            ["assortativity", "FHIR", "two-sided", "OpenEHR"],
+
+            ["cycles", "FHIR", "greater", "OpenEHR"],
+            ["cycles", "FHIR", "two-sided", "OpenEHR"],
+
+            ["avg_path_from_root", "FHIR", "less", "OpenEHR"],
+            ["avg_path_from_root", "FHIR", "two-sided", "OpenEHR"],
         ]
     
     test_results = []
-    for metric, is_one_side in graph_normal_distribution_metrics:
-        result = welch_test("standard", "FHIR", "OpenEHR", metric, is_one_side, DEFAULT_FILE_NAME)
+    for metric, key_value1, mode, key_value2 in graph_normal_distribution_metrics:
+        result = welch_test("standard", key_value1, key_value2, metric, mode, DEFAULT_FILE_NAME)
         test_results.append(result)
         display_result(result)
             
