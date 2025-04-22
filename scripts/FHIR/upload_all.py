b1aee1b3119210c439db44bd4dbaa2f3c558f14c
macioszczyk.mikolaj@gmail.com <macioszczyk.mikolaj@gmail.com>
2025-04-22 12:32:15 +0200

    upload all fhir

---
+++ scripts/FHIR/upload_all.py
@@ -0,0 +1,12 @@
+from upload_iniekcja import upload_iniekcja_full
+from upload_pomiar import upload_pomiar_full
+from upload_recepta_fhir import upload_recepta_full
+from upload_skierowanie_fhir import upload_skierowanie_full
+from upload_wyniki_badan import upload_wyniki_badan_full
+
+if __name__ == "__main__":
+    upload_iniekcja_full()
+    upload_pomiar_full()
+    upload_recepta_full()
+    upload_skierowanie_full()
+    upload_wyniki_badan_full()
