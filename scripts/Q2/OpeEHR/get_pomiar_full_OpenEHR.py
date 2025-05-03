b451eb077c062d7a55393cf7bad81bfd577c182d
macioszczyk.mikolaj@gmail.com <macioszczyk.mikolaj@gmail.com>
2025-05-03 19:03:01 +0200

    get other compositions OpenEHR

--- scripts/Q2/OpeEHR/get_pomiar_full_OpenEHR.py
+++ scripts/Q2/OpeEHR/get_pomiar_full_OpenEHR.py
@@ -1,14 +1,14 @@
 from openEHR_utils import find_ehr_by_subject_id, get_composition_details, get_latest_ehr_composition_id, save_composition_response
 
-PATIENT_PESEL = 80010112350
+PATIENT_PESEL = 80010112349
 
 if __name__ == "__main__":
     ehr_id = find_ehr_by_subject_id(PATIENT_PESEL)
     print(f"EHR id = {ehr_id}")
 
-    last_updated_id = get_latest_ehr_composition_id(ehr_id, "Progress Note")
-    print(f"Last updated Progress Note composition = {last_updated_id}")
+    last_updated_id = get_latest_ehr_composition_id(ehr_id, "Intraocular_pressure_study")
+    print(f"Last updated Intraocular_pressure_study composition = {last_updated_id}")
 
     composition = get_composition_details(ehr_id, last_updated_id)
-    save_composition_response(composition, "iniekcja.json")
-    print("Saved composition for iniekcja.")
+    save_composition_response(composition, "pomiar.json")
+    print("Saved composition for pomiar.")
--- scripts/Q2/OpeEHR/get_skierowanie_full_OpenEHR.py
+++ scripts/Q2/OpeEHR/get_skierowanie_full_OpenEHR.py
@@ -1,14 +1,14 @@
 from openEHR_utils import find_ehr_by_subject_id, get_composition_details, get_latest_ehr_composition_id, save_composition_response
 
-PATIENT_PESEL = 80010112350
+PATIENT_PESEL = 80010112346
 
 if __name__ == "__main__":
     ehr_id = find_ehr_by_subject_id(PATIENT_PESEL)
     print(f"EHR id = {ehr_id}")
 
-    last_updated_id = get_latest_ehr_composition_id(ehr_id, "Progress Note")
-    print(f"Last updated Progress Note composition = {last_updated_id}")
+    last_updated_id = get_latest_ehr_composition_id(ehr_id, "Referral document")
+    print(f"Last updated Referral document composition = {last_updated_id}")
 
     composition = get_composition_details(ehr_id, last_updated_id)
-    save_composition_response(composition, "iniekcja.json")
-    print("Saved composition for iniekcja.")
+    save_composition_response(composition, "skierowanie.json")
+    print("Saved composition for skierowanie.")
