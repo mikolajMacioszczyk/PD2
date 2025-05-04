f192a0715b87ddb64a186e1f8487bdd9416c16e9
macioszczyk.mikolaj@gmail.com <macioszczyk.mikolaj@gmail.com>
2025-05-04 11:20:55 +0200

    recepta simple queries

--- scripts/Q2/FHIR/get_recepta_full_FHIR.py
+++ scripts/Q2/FHIR/get_recepta_full_FHIR.py
@@ -1,4 +1,4 @@
-from fhir_utils import get_latest_resource_id_by_patient, get_patient_id_by_pesel, save_batch_response, send_batch_request
+from fhir_utils import get_latest_resource_id_by_patient, get_patient_id_by_pesel, get_resource, save_batch_response, send_batch_request
 
 PATIENT_PESEL = 80010112345
 
@@ -22,6 +22,19 @@ def create_get_full_recepta_batch_bundle(medication_request_id):
         ]
     }
 
+def get_pharmaceutical_form(observation_id):
+    resource_bundle = get_resource("MedicationRequest", observation_id, include="MedicationRequest:medication", elements="medication")
+    return resource_bundle["entry"][1]["resource"]["doseForm"]["text"]
+
+def get_frequency(observation_id):
+    resource_bundle = get_resource("MedicationRequest", observation_id, elements="dosageInstruction")
+    repeat = resource_bundle["entry"][0]["resource"]["dosageInstruction"][0]["timing"]["repeat"]
+    return f"{repeat['frequency']}/{repeat['durationUnit']}"
+
+def get_validity_period(observation_id):
+    resource_bundle = get_resource("MedicationRequest", observation_id, elements="dispenseRequest")
+    return resource_bundle["entry"][0]["resource"]["dispenseRequest"]["validityPeriod"]["end"]
+
 if __name__ == "__main__":
     patient_id = get_patient_id_by_pesel(PATIENT_PESEL)
     print(f"Patient id = {patient_id}")
@@ -34,3 +47,12 @@ if __name__ == "__main__":
     
     save_batch_response(batch_response, "recepta.json")
     print("Saved bundle for recepta.")
+
+    pharmaceutical_form = get_pharmaceutical_form(last_updated_resource_id)
+    print(f"Pharmaceutical form = {pharmaceutical_form}")
+
+    frequency = get_frequency(last_updated_resource_id)
+    print(f"Frequency = {frequency}")
+
+    validity_period = get_validity_period(last_updated_resource_id)
+    print(f"Validity period = {validity_period}")
--- scripts/Q2/OpeEHR/get_recepta_full_OpenEHR.py
+++ scripts/Q2/OpeEHR/get_recepta_full_OpenEHR.py
@@ -1,6 +1,28 @@
-from openEHR_utils import find_ehr_by_subject_id, get_composition_details, get_latest_ehr_composition_id, save_composition_response
+from openEHR_utils import find_ehr_by_subject_id, get_composition_details, get_latest_ehr_composition_id, get_property, save_composition_response
 
 PATIENT_PESEL = 80010112345
+
+def get_pharmaceutical_form(ehr_id, composition_id):
+    return get_property(ehr_id, 
+                        composition_id,
+                        archetype_type="CLUSTER",
+                        archetype_value="openEHR-EHR-CLUSTER.medication_substance.v0",
+                        property_path="items[at0133]/value/value")[0]
+
+def get_frequency(ehr_id, composition_id):
+    frequency = get_property(ehr_id, 
+                        composition_id,
+                        archetype_type="CLUSTER",
+                        archetype_value="openEHR-EHR-CLUSTER.timing_daily.v0",
+                        property_path="items[at0003]/value")[0]
+    return f"{frequency['magnitude']} {frequency['units']}"
+
+def get_validity_period(ehr_id, composition_id):
+    return get_property(ehr_id, 
+                        composition_id,
+                        archetype_type="CLUSTER",
+                        archetype_value="openEHR-EHR-CLUSTER.medication_authorisation.v0",
+                        property_path="items[at0072]/value/value")[0]
 
 if __name__ == "__main__":
     ehr_id = find_ehr_by_subject_id(PATIENT_PESEL)
@@ -12,3 +34,12 @@ if __name__ == "__main__":
     composition = get_composition_details(ehr_id, last_updated_id)
     save_composition_response(composition, "recepta.json")
     print("Saved composition for recepta.")
+
+    pharmaceutical_form = get_pharmaceutical_form(ehr_id, last_updated_id)
+    print(f"Pharmaceutical form = {pharmaceutical_form}")
+
+    frequency = get_frequency(ehr_id, last_updated_id)
+    print(f"Frequency = {frequency}")
+
+    validity_period = get_validity_period(ehr_id, last_updated_id)
+    print(f"Validity period = {validity_period}")
